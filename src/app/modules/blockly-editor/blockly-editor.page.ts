eb582c10fdbadd8763cac87bce0ed2bd37447d35
Paul <paul.siersma@snappet.org>
2019-12-12 20:22:39 +0100

    moving toolbox xml to component

--- src/app/modules/blockly-editor/blockly-editor.page.ts
+++ src/app/modules/blockly-editor/blockly-editor.page.ts
@@ -12,7 +12,21 @@ export class BlocklyEditorPage implements OnInit {
   constructor() {
 
     this.workspace = Blockly.inject('blocklyDiv', {
-      toolbox: document.getElementById('toolbox'),
+      toolbox: `<xml id="toolbox">
+      <category name="Control" colour="120">
+          <block type="controls_if"></block>
+          <block type="controls_repeat_ext" disabled="true"></block>
+      </category>
+      <category name="Text" colour="230">
+          <block type="text"></block>
+          <block type="text_print"></block>
+      </category>
+      <category name="Custom" colour="360">
+          <block type="begin"></block>
+          <block type="move"></block>
+          <block type="end"></block>
+      </category>
+  </xml>`,
       scrollbars: false
     });
 
