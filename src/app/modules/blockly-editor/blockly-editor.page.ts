e5a6f5454145dcfee529b72584cc39cbf79c686b
Paul <paul.siersma@snappet.org>
2019-12-13 10:55:12 +0100

    Minimum custom arduino block

--- src/app/modules/blockly-editor/blockly-editor.page.ts
+++ src/app/modules/blockly-editor/blockly-editor.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 declare var Blockly: any;
 
 @Component({
@@ -7,32 +7,32 @@ declare var Blockly: any;
   styleUrls: ['./blockly-editor.page.scss']
 })
 // tslint:disable-next-line: component-class-suffix
-export class BlocklyEditorPage implements OnInit {
+export class BlocklyEditorPage implements OnInit, AfterViewInit {
+  @ViewChild('blockContent', { static: false }) blockContent: ElementRef;
+
   private workspace: any;
   constructor() {
-
-    this.workspace = Blockly.inject('blocklyDiv', {
-      toolbox: `<xml id="toolbox">
-      <category name="Control" colour="120">
-          <block type="controls_if"></block>
-          <block type="controls_repeat_ext" disabled="true"></block>
-      </category>
-      <category name="Text" colour="230">
-          <block type="text"></block>
-          <block type="text_print"></block>
-      </category>
-      <category name="Custom" colour="360">
-          <block type="begin"></block>
-          <block type="move"></block>
-          <block type="end"></block>
-      </category>
-  </xml>`,
-      scrollbars: false
-    });
-
   }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.workspace = Blockly.inject(this.blockContent.nativeElement, {
+      toolbox: `<xml id="toolbox" style="display: none">
+      <category name="Control" colour="120">
+        <block type="controls_if"></block>
+        <block type="controls_repeat_ext" disabled="true"></block>
+      </category>
+      <category name="Text" colour="230">
+        <block type="text"></block>
+        <block type="text_print"></block>
+      </category>
+      <category name="Custom" colour="360">
+        <block type="arduino_base_inout_buildin_led"></block>
+      </category>
+    </xml>`,
+      scrollbars: false
+    });
+  }
 }
