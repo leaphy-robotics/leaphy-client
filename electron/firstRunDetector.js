79d592b1fddb4ae15265c5443bf85ea867147bd9
Jeroen Ooms <jooms@xebia.com>
2021-01-24 11:38:33 +0100

    Fix start screen

--- electron/firstRunDetector.js
+++ electron/firstRunDetector.js
@@ -1,17 +1,18 @@
 class FirstRunDetector {
-    constructor(firstRun){
-        this.firstRun = firstRun;
-    }
-    detectFirstRun = (event) => {
-        const isFirstRun = this.firstRun();
-        // Clear the firstRun setting, don't do this
-        //this.firstRun.clear();
+  constructor(firstRun) {
+    this.firstRun = firstRun;
+  }
+  detectFirstRun = (event) => {
+    //const isFirstRun = this.firstRun();
+    const isFirstRun = true;
+    // Clear the firstRun setting, don't do this
+    //this.firstRun.clear();
 
-        if(isFirstRun){
-            const firstRunDetectedMessage = { event: "FIRST_RUN" };
-            event.sender.send('backend-message', firstRunDetectedMessage);
-        }
+    if (isFirstRun) {
+      const firstRunDetectedMessage = { event: "FIRST_RUN" };
+      event.sender.send("backend-message", firstRunDetectedMessage);
     }
+  };
 }
 
 module.exports = FirstRunDetector;
