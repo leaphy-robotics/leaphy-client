699fa6f0a123a207a375de901dbdf92b6649b228
Bastiaan Bakker <bastiaan.bakker.osv@fedex.com>
2021-01-29 23:55:03 +0100

    move forge config to separate JS, add self-signed-cert, configure cert and password

---
+++ forge.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    icon: "./easybloqs-icon.ico"
+  },
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        loadingGif: "./easybloqs-loading.gif",
+        noMsi: true,
+        iconUrl: "file://easybloqs-icon.ico",
+        setupIcon: "./easybloqs-icon.ico",
+        certificateFile: process.env['WINDOWS_CODESIGN_CERTIFICATE'],
+        certificatePassword: process.env['WINDOWS_CODESIGN_PASSWORD']
+      }
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: [
+        "darwin",
+        "linux"
+      ]
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        iconUrl: "file://easybloqs-icon.ico"
+      }
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {}
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {}
+    }
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "leaphy-robotics",
+          name: "leaphy-client"
+        },
+        prerelease: true,
+        draft: true
+      }
+    }
+  ]
+}
