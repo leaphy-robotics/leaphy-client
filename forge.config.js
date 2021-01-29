f39feabe5b6ad7f187d5379de21e050370828e4c
Bastiaan Bakker <bastiaan.bakker.osv@fedex.com>
2021-01-29 23:55:03 +0100

    move forge config to separate JS, add self-signed-cert, configure cert and password

---
+++ forge.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  "packagerConfig": {
+    "asar": true,
+    "icon": "./easybloqs-icon.ico"
+  },
+  "makers": [
+    {
+      "name": "@electron-forge/maker-squirrel",
+      "config": {
+        "setupExe": "EasyBloqsSetup.exe",
+        "loadingGif": "./easybloqs-loading.gif",
+        "noMsi": true,
+        "iconUrl": "file://easybloqs-icon.ico",
+        "setupIcon": "./easybloqs-icon.ico"
+        "certificateFile": "./self-signed-cert.pfx",
+        "certificatePassword": process.env['CERTIFICATE_PASSWORD']
+      }
+    },
+    {
+      "name": "@electron-forge/maker-zip",
+      "platforms": [
+        "darwin",
+        "linux"
+      ]
+    },
+    {
+      "name": "@electron-forge/maker-dmg",
+      "config": {
+        "iconUrl": "file://easybloqs-icon.ico"
+      }
+    },
+    {
+      "name": "@electron-forge/maker-deb",
+      "config": {}
+    },
+    {
+      "name": "@electron-forge/maker-rpm",
+      "config": {}
+    }
+  ],
+  "publishers": [
+    {
+      "name": "@electron-forge/publisher-github",
+      "config": {
+        "repository": {
+          "owner": "leaphy-robotics",
+          "name": "leaphy-client"
+        },
+        "prerelease": true,
+        "draft": true
+      }
+    }
+  ]
+}
