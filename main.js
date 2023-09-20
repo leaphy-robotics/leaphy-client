1d18c606cc01b50b586bbc3c3efaa8194d884afc
Jefta <jeftadegraaff@gmail.com>
2023-09-20 21:26:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -146,5 +146,5 @@ function createWindow() {
     require("@electron/remote/main").enable(mainWindow.webContents);
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 }
