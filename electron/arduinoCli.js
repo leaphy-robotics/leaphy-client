5d516e4674bfcaca71459dac4c361688fffad7e4
vosjedev <hetlachendevosje@gmail.com>
2024-01-16 18:15:42 +0100

    Change the way platform is found (linux arm64 support) This makes the way platforms are detected more advanced, to allow more options than windows or linux.

--- electron/arduinoCli.js
+++ electron/arduinoCli.js
@@ -8,7 +8,26 @@ class ArduinoCli {
 
     getArduinoCliPath = (os, path, app) => {
         const platform = os.platform()
-        const arduino_cli = (platform == "win32") ? "arduino-cli.exe" : "arduino-cli";
+        // const arduino_cli = (platform == "win32") ? "arduino-cli.exe" : "arduino-cli";
+        let arduino_cli = 'arduino-cli'
+        if (platform == "win32") { // overwrites
+            console.log("Found windows")
+            arduino_cli = "arduino-cli.exe"
+        } else if (platform == "linux") {
+            console.log("Found linux")
+            const arch = process.arch
+            if (arch == "x64") { // generic x86_64 system
+                arduino_cli = "arduino-cli"
+            } else if (arch == "arm64") { // all arm64 systems
+                arduino_cli = "arduino-cli-arm64"
+            } else {
+                console.log("Unhandeled architecture `"+arch+"`! Assuming generic x86_64 system.")
+                console.log("Please make a bug report tagging @vosjedev on")
+                console.log("https://github.com/leaphy-robotics/leaphy-client/issues/")
+                console.log("for support for your architecture. Also include this message, and your systems architecture.")
+                arduino_cli = "arduino-cli"
+            }
+        }
         return path.join(app.getAppPath(), 'lib', platform, 'arduino_cli', arduino_cli);
     }
 
